b020969657ee34bed520041f79d22c99e955957b
hernan verar <hernanverar@gmail.com>
2023-08-04 09:45:20 -0700

    This commit add oour first migrations engineer MVC adn updates Factory.csproj

---
+++ Factory/Models/EngineerMachine.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class EngineerMachine
+  {
+    public int EngineerId { get; set; }
+      public Engineer Engineer { get; set; }
+  }
+}    
