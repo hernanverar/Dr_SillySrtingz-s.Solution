44d826611145633a6ad543a3c6e8ffca8dcca8be
hernan verar <hernanverar@gmail.com>
2023-08-13 15:12:18 -0700

    This commit updates our Css styles Layouts

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -6,7 +6,7 @@ namespace Factory.Models
   public class Machine
   {
     public int MachineId { get; set; }
-    [Required(ErrorMessage = "A Machine with no name? Bah! This field can't be empty!")]
+    [Required(ErrorMessage = "Add Machine This field can't be empty!")]
     public string Designation { get; set; }
     public List<EngineerMachine> JoinEntities { get; set; }
   }
